801715575d20ed7d71be8600327229c771c632cc
Vladislav <legovladislav@gmail.com>
2023-02-13 16:57:38 +0300

    Добавил перемещение камеры за персонажем. Чуть расширил количество тайлов воды.

---
+++ arena/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform following;
+        [SerializeField] private float offsetZ;
+        
+        private void LateUpdate()
+        {
+            if (following == null)
+            {
+                return;
+            }
+
+            Vector3 position = following.position;
+            position.z += offsetZ;
+
+            transform.position = position;
+        }
+    }
+}
